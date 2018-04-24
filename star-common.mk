3ce14202b0683253a680dc57ec46558dcc3ef219
Jesse Chan <jc@lineageos.org>
2018-04-23 17:50:14 -0700

    star: add CarbonROM support
    Change-Id: I30b0a784655119a75821bd34bb1e704d509f5b3a

--- star-common.mk
+++ star-common.mk
@@ -7,6 +7,12 @@ ifneq ($(filter lineage_starlte lineage_star2lte, $(TARGET_PRODUCT)),)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 endif
 
+ifneq ($(findstring carbon, $(TARGET_PRODUCT)),)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-carbon
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.carbon.maintainer="vaughnn"
+endif
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
